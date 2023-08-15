85c657f802b658edafca683f2e03d503298c8dbe
franzromanov <114895290+franzromanov@users.noreply.github.com>
2023-08-15 14:42:28 +0800

    Update F_ADD.v

--- COMBINATIONAL/F_ADD.v
+++ COMBINATIONAL/F_ADD.v
@@ -1,33 +1,14 @@
-/*
-    FULL ADDER_Module
-	 
-    Copyright (C) 2023  Michael Navallo Higgins
+module F_ADD(a,b,carryIN,carryOUT,sum);
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
-//full_adder
-module f_add(a,b,carryIN,carryOUT,sum);
 //declare
- input a,b,carryIN;
- output carryOUT,sum;
- wire w1,w2,w3;
+  input a,b,carryIN;
+  output carryOUT,sum;
+  wire w1,w2,w3;
  
 //operation;
- h_add();
-
-
+  H_ADD H_main(a,b,w1,w2);
+  H_ADD H_carry(carryIN,w1,sum,w3);
+  or _ORgate(carryOUT,w2,w3);
 
 //esc
 endmodule
